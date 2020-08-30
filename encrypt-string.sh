11f198927443129a46058133329bc441731ab6fe
James Lu <james@overdrivenetworks.com>
2020-08-29 23:28:53 -0700

    encrypt-string: remove redundant --vault-password-file

--- encrypt-string.sh
+++ encrypt-string.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 read -r -s -p "Enter string to encrypt: " string
-echo -n "$string" | ansible-vault encrypt_string --vault-password-file ~/ansible-dn42.key --stdin-name "${1:-"unnamed"}"
+echo -n "$string" | ansible-vault encrypt_string --stdin-name "${1:-"unnamed"}"
