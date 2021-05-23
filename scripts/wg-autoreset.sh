a066ae6ca334c3de264e1eb0053a0fb78eff68f4
James Lu <james@overdrivenetworks.com>
2021-05-22 19:25:10 -0700

    wg-autoreset: always do a hard reset
    For some reason, using wg setconf causes the tunnel to never come back up again, failing with an "Invalid MAC of handshake" error each time.

--- scripts/wg-autoreset.sh
+++ scripts/wg-autoreset.sh
@@ -10,24 +10,25 @@ read -ra wg_interfaces < <(wg show interfaces)
 
 TIMEOUT=$(expr 60 '*' 30)  # 30 minutes
 
+reset_iface() {
+    echo "  Resetting interface $1"
+    ip link del "$1"
+    ifup "$1" --force
+    echo "  Done"
+}
+
 for iface in "${wg_interfaces[@]}"; do
     last_handshake="$(wg show "$iface" latest-handshakes)"
     if [[ -z "$last_handshake" ]]; then
         echo "$iface: last handshake field is empty (interface probably failed to start)"
-        echo "  Hard resetting interface"
-        ip link del "$iface"
-        ifup "$iface"
-        echo "  Done"
+        reset_iface "$iface"
         continue
     fi
 
     last_handshake_time="$(cut -f 2 <<< "$last_handshake")"
     if (($(date +%s) - $last_handshake_time > $TIMEOUT)); then
         echo "$iface: last handshake $last_handshake_time is longer than ${TIMEOUT}s ago"
-        echo "  Soft resetting interface"
-        # XXX: hardcoded path
-        wg syncconf "$iface" "/etc/wireguard/$iface.conf"
-        echo "  Done"
+        reset_iface "$iface"
     else
         echo "$iface: OK"
     fi
