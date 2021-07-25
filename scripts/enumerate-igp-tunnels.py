ae03ce8c54746a755a47336bc7d604525feb1287
James Lu <james@overdrivenetworks.com>
2021-07-25 12:41:19 -0700

    Reallocate IGP port numbers to 3xxxx
    Also, refactor enumerate-igp-tunnels to only allocate ports for enabled links

--- scripts/enumerate-igp-tunnels.py
+++ scripts/enumerate-igp-tunnels.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """
-Enumerate intra-AS tunnel settings for AS4242421080. This script generates a list of intra-AS neighbours for each node (mesh and non-mesh), as well as which Wireguard ports to connect to.
+Enumerate intra-AS tunnel settings for AS4242421080. This script generates a list of intra-AS
+neighbours for each node (mesh and non-mesh), as well as which Wireguard ports to connect to.
 This was split from the Ansible playbook to allow for more flexibility.
 """
-import itertools
 import os
 import pprint
 
 from _common import *
 
 OUTFILE = "global-config/igp-tunnels.yml"
-START_PORT = 55000  # Port to start building tunnels from
+START_PORT = 30000  # Port to start building tunnels from
 
 def _read_previous_settings():
     if os.path.exists(OUTFILE):
@@ -24,55 +24,61 @@ def _read_previous_settings():
             'igp_wg_ports': {},
         }
 
+def _add_tunnel(data, server1, server2):
+    """
+    Add a link between server1 and server2, and assign a new VPN port for this pair
+    if one does not already exist.
+    """
+    if server1 == server2:
+        return
+    neighbours1 = data['igp_neighbours'].setdefault(server1, set())
+    neighbours2 = data['igp_neighbours'].setdefault(server2, set())
+    neighbours1.add(server2)
+    neighbours2.add(server1)
+
+    wg_ports1 = data['igp_wg_ports'].setdefault(server1, {})
+    wg_ports2 = data['igp_wg_ports'].setdefault(server2, {})
+    next_port = data['next_port']
+    if server2 in wg_ports1:  # a port is already assigned; skip
+        return
+    wg_ports1[server2] = next_port
+    wg_ports2[server1] = next_port
+
+    data['next_port'] += 1
+
 def main():
     data = _read_previous_settings()
     hosts = yaml_load('hosts.yml')
 
     dn42routers = get_hosts(hosts)
-    meshrouters = hosts['meshrouters']['hosts']
-    data['igp_neighbours'].clear()  # clear igp_neighbours, we will be rewriting it
+    meshrouters = set(hosts['meshrouters']['hosts'])
+    # Between runs, the desired list of neighbours for a node can change, so we clear it here and rewrite it.
+    # VPN port allocations however do stick around even as links are removed, in order to ensure stability.
+    data['igp_neighbours'].clear()
     seen_shortnames = {}
 
-    # For each router, populate a list of neighbours
-    for server, serverdata in dn42routers.items():
-        neighbours = data['igp_neighbours'].setdefault(server, set())
-
+    for server1, serverdata in dn42routers.items():
         # Sanity check: make sure there are no duplicate shortnames
         shortname = serverdata['shortname']
         if shortname in seen_shortnames:
-            raise ValueError(f"Duplicate shortname {shortname}: {seen_shortnames[shortname]}, {server}")
-        seen_shortnames[shortname] = server
+            raise ValueError(f"Duplicate shortname {shortname}: {seen_shortnames[shortname]}, {server1}")
+        seen_shortnames[shortname] = server1
 
-        if server in meshrouters:
-            # For servers in meshrouters group, add all other nodes in the mesh
-            neighbours |= set(meshrouters)
-            neighbours.remove(server)
+        if server1 in meshrouters:
+            for server2 in meshrouters:
+                _add_tunnel(data, server1, server2)
 
         # For leaf servers, add all nodes specified in igp_upstreams
         igp_upstreams = set(serverdata.get('igp_upstreams', []))
 
         # ^ in this case is logical xor
-        if not ((server in meshrouters) ^ bool(igp_upstreams)):
-            raise ValueError(f'{server} must either define igp_upstreams or be part of meshrouters group (and not both)')
+        if not (server1 in meshrouters) ^ bool(igp_upstreams):
+            raise ValueError(f'{server1} must either define igp_upstreams or be part of meshrouters group '
+                              '(and not both)')
 
-        neighbours |= igp_upstreams
-        for neighbour in igp_upstreams:
-            # Add ourselves as neighbour to all of our upstreams
-            data['igp_neighbours'].setdefault(neighbour, set()).add(server)
+        for server2 in igp_upstreams:
+            _add_tunnel(data, server1, server2)
 
-    # Now populate ports for each combination of routers. This simplfies by including all routers, even those that aren't directly connected
-    ports = data['igp_wg_ports']
-    for cmb in itertools.combinations(dn42routers, 2):
-        # Join tuples together as they are not a native YAML type
-        cmb_reverse = ','.join((cmb[1], cmb[0]))
-        cmb = ','.join(cmb)
-        if cmb not in ports or cmb_reverse not in ports:
-            # Only add combinations we haven't seen before
-            print(f"Adding port {data['next_port']} for IGP link {cmb}")
-            ports[cmb] = ports[cmb_reverse] = data['next_port']
-            data['next_port'] += 1
-        else:
-            print(f"Using existing port {ports[cmb]} for IGP link {cmb}")
 
     pprint.pprint(data)
     with open(OUTFILE, 'w') as f:
