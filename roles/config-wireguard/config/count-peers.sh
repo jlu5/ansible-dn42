a504fca36d12b0645886730468a91773ea6196ff
James Lu <james@overdrivenetworks.com>
2022-05-18 22:24:33 -0700

    various: add baiyu / AS4242421901

--- roles/config-wireguard/config/count-peers.sh
+++ roles/config-wireguard/config/count-peers.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 for f in *.yml; do
 	echo "$f"
-	yq '[.wg_peers[] | select( .remove == null ) | select( .name | startswith("dn42") )] | length' "$f"
+	yq '[.wg_peers[] | select( .remove == null )] | length' "$f"
 done
