68ca6af9c3c20d8dfabe10544a6ea1a424b32976
James Lu <james@overdrivenetworks.com>
2022-06-02 11:53:20 -0700

    update-roas: allow reconfigure step to fail

--- roles/setup-base/files/dn42-update-roas.sh
+++ roles/setup-base/files/dn42-update-roas.sh
@@ -4,4 +4,4 @@ set -e
 curl -fSLR {-o,-z}/etc/bird/roa_dn42.conf    https://dn42.burble.com/roa/dn42_roa_bird2_4.conf
 curl -fSLR {-o,-z}/etc/bird/roa_dn42_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf
 sed -i 's/roa/route/g' /etc/bird/roa_dn42{,_v6}.conf
-/usr/sbin/birdc configure
+/usr/sbin/birdc configure || true
