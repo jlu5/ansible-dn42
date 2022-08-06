6e3799df62624530280fbf8d5b4e28b1f36a7eda
James Lu <james@overdrivenetworks.com>
2022-08-06 15:54:05 -0700

    routestats: count # of unique destination ASNs per peering

--- scripts/routestats.py
+++ scripts/routestats.py
@@ -3,72 +3,66 @@
 Routestats: count BIRD route stats
 """
 
+import collections
 import json
 import subprocess
 import re
 import sys
 import time
 
-BIRD_PROTO_RE = re.compile(r'^(\w+?)\s+(\w+)')
-BIRD_ROUTE_COUNT_RE = re.compile(r'\s+Routes:\s+(\d+) imported, (\d+) exported, (\d+) preferred')
 IBGP_SUFFIX = "ibgp_"
+BIRD_ROUTE_RE = re.compile(r'^([0-9a-f.:/]+)\s+unicast \[(\w+) .*?\].*\[(AS\d+)i\]')
 
-def get_stats():
-    stats4 = {}
-    stats6 = {}
-    total4 = total6 = 0
+def get_stats(table):
+    n_prefixes_by_session = collections.defaultdict(int)
+    total_prefixes = 0
+    target_asns_by_session = collections.defaultdict(set)
 
-    cmd = ["/usr/sbin/birdc", "show pr all"]
+    cmd = ["/usr/sbin/birdc", "show r primary table " + table]
 
     bird_output = subprocess.check_output(cmd).decode('utf-8')
-
-    curr_session = None
-    found_v4_header = found_v6_header = False
     for line in bird_output.splitlines():
-        if match := BIRD_PROTO_RE.match(line):
-            curr_session, protocol = match.groups()
-            found_v4_header = found_v6_header = False
-            if protocol != 'BGP':
-                curr_session = None
-                continue
-            print(f"Checking session {curr_session}, type {protocol}", file=sys.stderr)
+        if match := BIRD_ROUTE_RE.match(line):
+            prefix, session_name, origin_asn = match.groups()
+            target_asns_by_session[session_name].add(origin_asn)
+            n_prefixes_by_session[session_name] += 1
+            total_prefixes += 1
 
-        if curr_session:
-            if line.lstrip() == 'Channel ipv4':
-                found_v4_header = True
-                continue
-            if line.lstrip() == 'Channel ipv6':
-                found_v4_header = False  # ipv6 always comes after
-                found_v6_header = True
-                continue
-            if match := BIRD_ROUTE_COUNT_RE.match(line):
-                n_imported, n_exported, n_preferred = map(int, match.groups())
-                assert found_v4_header ^ found_v6_header
-                if found_v4_header:
-                    total4 += n_preferred
-                    stats4[curr_session] = n_preferred
-                    found_v4_header = False
-                    print(f"  Using {n_preferred}/{n_imported} IPv4 routes from {curr_session}", file=sys.stderr)
-                elif found_v6_header:
-                    total6 += n_preferred
-                    stats6[curr_session] = n_preferred
-                    found_v6_header = False
-                    print(f"  Using {n_preferred}/{n_imported} IPv6 routes from {curr_session}", file=sys.stderr)
+    return total_prefixes, n_prefixes_by_session, target_asns_by_session
 
-    return total4, stats4, total6, stats6
+def _sort(d):
+    return sorted(d, key=lambda pair: pair[1], reverse=True)
 
-total4, stats4, total6, stats6 = get_stats()
-stats4 = sorted(stats4.items(), key=lambda pair: pair[1], reverse=True)
-stats6 = sorted(stats6.items(), key=lambda pair: pair[1], reverse=True)
+def _get_igp_links(d):
+    return [i for i, x in enumerate(d) if x[0].startswith(IBGP_SUFFIX)]
 
-out = json.dumps({
-    "ipv4": stats4,
-    "ipv4_igp_links": [i for i, x in enumerate(stats4) if x[0].startswith(IBGP_SUFFIX)],
-    "ipv4_total": total4,
-    "ipv6": stats6,
-    "ipv6_igp_links": [i for i, x in enumerate(stats6) if x[0].startswith(IBGP_SUFFIX)],
-    "ipv6_total": total6,
-    "ts": int(time.time())
-})
+def main():
+    n_prefixes4, stats_by_prefix4, asns4 = get_stats(table="master4")
+    n_prefixes6, stats_by_prefix6, asns6 = get_stats(table="master6")
+    stats_by_prefix4 = _sort(stats_by_prefix4.items())
+    stats_by_prefix6 = _sort(stats_by_prefix6.items())
+    asns4 = _sort([(k, len(v)) for k, v in asns4.items()])
+    asns6 = _sort([(k, len(v)) for k, v in asns6.items()])
 
-print(out)
+    out = json.dumps({
+        "ipv4": stats_by_prefix4,
+        "ipv4_igp_links": _get_igp_links(stats_by_prefix4),
+        "ipv4_total": n_prefixes4,
+
+        "ipv4_asns": asns4,
+        "ipv4_asns_igp_links": _get_igp_links(stats_by_prefix4),
+
+        "ipv6": stats_by_prefix6,
+        "ipv6_igp_links": _get_igp_links(asns4),
+        "ipv6_total": n_prefixes6,
+
+        "ipv6_asns": asns6,
+        "ipv6_asns_igp_links": _get_igp_links(asns6),
+
+        "ts": int(time.time())
+    })
+
+    print(out)
+
+if __name__ == '__main__':
+    main()
